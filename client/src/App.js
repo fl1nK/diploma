a9a21cbd9f601e42766b379c95e3be1dde7658ad
Vlad Filonenko <filonenkovlad5@gmail.com>
2024-04-21 20:36:13 +0300

    процююча ші реакт

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import "./App.css";
 import * as faceapi from "face-api.js";
 
 function App() {
   const videoRef = useRef();
   const canvasRef = useRef();
+
+  const [people, setPeople] = useState();
 
   // LOAD FROM USEEFFECT
   useEffect(() => {
@@ -28,49 +30,101 @@ function App() {
   const loadModels = () => {
     Promise.all([
       // THIS FOR FACE DETECT AND LOAD FROM YOU PUBLIC/MODELS DIRECTORY
+
+      faceapi.nets.ssdMobilenetv1.loadFromUri("/models"),
       faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
       faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
       faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
-      faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+      // faceapi.nets.faceExpressionNet.loadFromUri("/models"),
     ]).then(() => {
       faceMyDetect();
     });
   };
 
-  const faceMyDetect = () => {
+  function getLabeledFaceDescriptions() {
+    const labels = ["vlad"];
+    return Promise.all(
+      labels.map(async (label) => {
+        const descriptions = [];
+        for (let i = 1; i <= 2; i++) {
+          const img = await faceapi.fetchImage(`/labels/${label}/${i}.png`);
+          // const img = await faceapi.fetchImage(`D:/Диплом/diploma/server/labels/${label}/${i}.png`);
+          const detections = await faceapi
+            .detectSingleFace(img)
+            .withFaceLandmarks()
+            .withFaceDescriptor();
+          descriptions.push(detections.descriptor);
+        }
+        return new faceapi.LabeledFaceDescriptors(label, descriptions);
+      })
+    );
+  }
+
+  const faceMyDetect = async () => {
+    const labeledFaceDescriptors = await getLabeledFaceDescriptions();
+    const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors);
+
+    // const canvas = faceapi.createCanvasFromMedia(videoRef.current);
+
     setInterval(async () => {
       const detections = await faceapi
         .detectAllFaces(videoRef.current, new faceapi.TinyFaceDetectorOptions())
         .withFaceLandmarks()
-        .withFaceExpressions();
+        .withFaceDescriptors();
+      // .withFaceExpressions();
 
       // DRAW YOU FACE IN WEBCAM
       canvasRef.current.innerHtml = faceapi.createCanvasFromMedia(
         videoRef.current
       );
       faceapi.matchDimensions(canvasRef.current, {
-        width: 940,
-        height: 650,
+        width: 640,
+        height: 480,
       });
 
       const resized = faceapi.resizeResults(detections, {
-        width: 940,
-        height: 650,
+        width: 640,
+        height: 480,
       });
 
-      faceapi.draw.drawDetections(canvasRef.current, resized);
-      faceapi.draw.drawFaceLandmarks(canvasRef.current, resized);
-      faceapi.draw.drawFaceExpressions(canvasRef.current, resized);
+      canvasRef.current
+        .getContext("2d", { willReadFrequently: true })
+        .clearRect(0, 0, 640, 480);
+
+      const results = resized.map((d) => {
+        return faceMatcher.findBestMatch(d.descriptor);
+      });
+
+      // faceapi.draw.drawDetections(canvasRef.current, results);
+      // faceapi.draw.drawFaceLandmarks(canvasRef.current, results);
+      // faceapi.draw.drawFaceExpressions(canvasRef.current, results);
+
+      results.forEach((result, i) => {
+        // console.log(result.label);
+        setPeople(result.label);
+        const box = resized[i].detection.box;
+        const drawBox = new faceapi.draw.DrawBox(box, {
+          label: result,
+        });
+        drawBox.draw(canvasRef.current);
+      });
     }, 1000);
   };
 
   return (
     <div className="myapp">
-      <h1>FAce Detection</h1>
+      <div>
+        <h2>{people}</h2>
+      </div>
       <div className="appvide">
         <video crossOrigin="anonymous" ref={videoRef} autoPlay></video>
+        <canvas
+          ref={canvasRef}
+          width="640"
+          height="480"
+          className="appcanvas"
+        />
       </div>
-      <canvas ref={canvasRef} width="940" height="650" className="appcanvas" />
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const fs = require("fs");
+const path = require("path");
 
 const { exportUser, createUser } = require("./controller/User");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -13,14 +15,25 @@ app.use(express.urlencoded({ extended: false }));
 mongoose.set("strictQuery", false);
 mongoose
   .connect(
-    "mongodb+srv://vlad:pass123@cluster0.g8rarqs.mongodb.net/diplom?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useUnifiedTopology: true }
+    "mongodb+srv://vlad:pass123@cluster0.g8rarqs.mongodb.net/diplom?retryWrites=true&w=majority"
   )
   .then(() => console.log("Connected to DB"))
   .catch((error) => console.log(error));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+// Шлях до папки для зберігання фото
+const photoDirectory = path.join(__dirname, "photos");
+
+// Маршрут для отримання фото за посиланням
+app.get("/photos/:photoName", (req, res) => {
+  const photoName = req.params.photoName;
+  const photoPath = path.join(photoDirectory, photoName);
+
+  // Відправляємо фото у відповідь
+  res.sendFile(photoPath);
 });
 
 app.get("/downloadExcel", exportUser);
