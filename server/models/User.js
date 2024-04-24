bac17e72551ff298c6a612fd0cb2a791ec3c231b
Vlad Filonenko <v.filonenko@kovalska.com>
2024-04-24 17:00:08 +0300

    added face-api to server

--- server/models/User.js
+++
@@ -1,8 +0,0 @@
-const { Schema, model } = require("mongoose");
-
-const User = new Schema({
-  first_name: { type: String, default: "" },
-  last_name: { type: String, default: "" },
-});
-
-module.exports = model("User", User);
